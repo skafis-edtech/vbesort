793f14b5206ab73925f464554394ebdc7b59482c
Naglis <naglis1134@gmail.com>
2024-01-13 21:54:43 +0200

    daugiau parusiuot

--- src/MainPage/components/InfoComponent.tsx
+++ src/MainPage/components/InfoComponent.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Support from "../../components/Support";
+import { Alert } from "react-bootstrap";
 
 export default function InfoComponent() {
   return (
@@ -9,6 +10,11 @@ export default function InfoComponent() {
         surūšiuotos pagal temas. Viskas nemokama. Naudokitės į valias. Plačiau
         skaitykite <Link to="/about">puslapyje "Apie".</Link>
       </p>
+      <Alert>
+        Ieškau užduočių/atsakymų screenshotintojo(-os), rūšiuotojo(-os) (ne už
+        ačiū). Dėl daugiau informacijos -{" "}
+        <a href="mailto:naglis.suliokas@gmail.com">susisiekti</a>
+      </Alert>
       <Support />
     </div>
   );
